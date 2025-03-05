3b12772ffd0b132201a39de6a9eaaeff0478f52c
zhengyb <zhengyb@inhand.com.cn>
2025-03-04 21:07:57 -0500

    rewrite camera name

--- tools/camera_geometry.py
+++ tools/camera_geometry.py
@@ -1,8 +1,11 @@
 import numpy as np
+from tools.utils import CameraName
 
 def get_intrinsic_matrix4carla(image_width, image_height, field_of_view_deg=45):
     # For our Carla camera alpha_u = alpha_v = alpha
     # alpha can be computed given the cameras field of view via
+    if field_of_view_deg is None:
+        raise ValueError("field_of_view_deg must be provided for Carla camera")
     field_of_view_rad = field_of_view_deg * np.pi/180
     alpha = (image_width / 2.0) / np.tan(field_of_view_rad / 2.)
     Cu = image_width / 2.0
@@ -27,7 +30,8 @@ def project_polyline(polyline_world, trafo_world_to_cam, K):
 
 
 class CameraGeometry(object):
-    def __init__(self, camera_name, height=1.3, yaw_deg=0, pitch_deg=-5, roll_deg=0, image_width=1920, image_height=1080):
+    def __init__(self, camera_name, height=1.3, yaw_deg=0, pitch_deg=-5, roll_deg=0, image_width=1920, image_height=1080,
+                 field_of_view_deg=None):
         # scalar constants
         self.camera_name = camera_name
         self.height = height
@@ -36,9 +40,10 @@ class CameraGeometry(object):
         self.yaw_deg = yaw_deg
         self.image_width = image_width
         self.image_height = image_height
+        self.field_of_view_deg = field_of_view_deg  # 45
         # camera intriniscs and extrinsics
-        if self.camera_name == "carla_camera":
-            self.intrinsic_matrix = get_intrinsic_matrix4carla(self.image_width, self.image_height)
+        if self.camera_name == CameraName.CARLA or self.camera_name == CameraName.BEV:
+            self.intrinsic_matrix = get_intrinsic_matrix4carla(self.image_width, self.image_height, self.field_of_view_deg)
         else:
             self.intrinsic_matrix = get_intrinsic_matrix(self.camera_name)
         self.inverse_intrinsic_matrix = np.linalg.inv(self.intrinsic_matrix)
@@ -59,10 +64,16 @@ class CameraGeometry(object):
         self.trafo_cam_to_road[0:3,3] = self.translation_cam_to_road
         # compute vector nc. Note that R_{rc}^T = R_{cr}
         self.road_normal_camframe = self.rotation_cam_to_road.T @ np.array([0,1,0])
+        self.uv_to_roadXYZ_iso8855_tbl = []
+        self.roadXYZ_iso8855_to_uv_tbl = []
+        self.cut_v = 0
 
 
     def camframe_to_roadframe(self,vec_in_cam_frame):
         return self.rotation_cam_to_road @ vec_in_cam_frame + self.translation_cam_to_road
+
+    def roadframe_to_camframe(self,vec_in_road_frame):
+        return self.rotation_cam_to_road.T @ (vec_in_road_frame - self.translation_cam_to_road)
 
     def uv_to_roadXYZ_camframe(self,u,v):
         # NOTE: The results depend very much on the pitch angle (0.5 degree error yields bad result)
